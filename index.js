55985836b05a95a57260bfa9acd0500e4230d974
harold <mwijukaharoid@gmail.com>
2023-12-16 10:14:18 +0300

    commit #1. Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const num1 = Math.floor(Math.random()*10);
+const num2 = Math.floor(Math.random()*10);
+
+const quesEl = document.getElementById("question");
+const formEl = document.getElementById("form");
+const inputEl =document.getElementById("input");
+const scoreEl = document.getElementById("score");
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+if (!score) {
+    score = 0
+};
+
+
+scoreEl.innerText = `score: ${score}`;
+
+
+quesEl.innerText = `What is ${num1} multiply by ${num2} `
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit", ()=>{
+    const userAns = +inputEl.value 
+    if(userAns === correctAns) {
+        score++
+        updatelocalStorage()
+    }
+     else if (userAns !== correctAns){
+        score--
+        updatelocalStorage()
+    } 
+});
+
+/*formEl.addEventListener("newGame", ()=>{
+
+}) */
+
+function updatelocalStorage (){
+    localStorage.setItem('score', JSON.stringify(score))
+}
+
